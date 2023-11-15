31c1854900c10cf8b1471be77926676af09fde77
fizzayscode <hbumar100@gmail.com>
2023-11-15 16:45:08 +0100

    inital commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  console.log(count);
+  return (
+    <>
+      <h1 className="text-center  text-red-700 font-bold">Hello world </h1>
+    </>
+  );
+}
+
+export default App;
