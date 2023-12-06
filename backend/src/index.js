887f70c523a3babc15bb3c1c51790ffeca753621
fizzayscode <hbumar100@gmail.com>
2023-12-06 16:36:54 +0100

    hot fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,10 +8,13 @@ require("dotenv").config();
 const dirname = path.resolve();
 const app = express();
 app.use(express.json());
-const allowedOrigin = "http://localhost:5173";
+const allowedOrigin = [
+  "http://localhost:5173",
+  "https://fizzays-mern-estae.onrender.com/",
+];
 const corsOptions = {
   origin: function (origin, callback) {
-    if (origin === allowedOrigin || !origin) {
+    if (allowedOrigin.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
