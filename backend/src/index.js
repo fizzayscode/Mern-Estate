9cfa512a52ed2e3d300c5240e2602f1389e385c8
fizzayscode <hbumar100@gmail.com>
2023-12-06 15:42:53 +0100

    latest update

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const appRouter = require("./routes/index");
+const path = require("path");
 const cors = require("cors");
 const errorHandlerMiddleWare = require("./Middleware/errorHandler");
 require("dotenv").config();
-
+const dirname = path.resolve();
 const app = express();
 app.use(express.json());
 const allowedOrigin = "http://localhost:5173";
@@ -19,12 +20,16 @@ const corsOptions = {
   credentials: true,
   optionsSuccessStatus: 200,
 };
-
 app.use(cors(corsOptions));
+app.use(express.static(path.join(dirname, "/frontend/dist")));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use("/api/v1", appRouter);
+app.get("*", (req, res) => {
+  res.sendFile(path.join(dirname, "frontend", "dist", "index.html"));
+});
 app.use(errorHandlerMiddleWare);
 
+// any file i go to that is not api/v1/ it should send this html file
 app.listen(process.env.PORT, () => {
   console.log("running on port 8080");
 });
